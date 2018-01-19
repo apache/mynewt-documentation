896fadcd46f3fe7fe6639a91931d26482dd59368
Gavin Jefferies <gavin@runtime.io>
2018-01-19 08:28:53 -0800

    remove unused js

--- docs/themes/mynewt/static/js/main.js
+++ docs/themes/mynewt/static/js/main.js
@@ -1,10 +1,4 @@
 jQuery(document).ready(function($) {
-	$("span#secondary-menu-button").click(function() {
-		if ($(window).width() < 768) {
-			$("div.container-sidebar").toggleClass("-expanded-submenu");
-		}
-	});
-
 	getNavbarOffset = function() {
 		return $("#banner").height();
 	};
@@ -18,36 +12,8 @@ jQuery(document).ready(function($) {
 	});
 });
 
-function manageDocSidebar() {
-	if (window.matchMedia("(max-width: 992px)").matches) {
-		// menu above content
-		$("#docSidebar").css("height", "initial");
-		$("#docSidebar").css("overflow-y", "initial");
-	} else {
-		// side by side - menu + content
-		var sidebarHeight = $("#docSidebar").height();
-		console.log("sidebar at " + sidebarHeight);
-
-		var visibleHeight = $(window).height() - $("#navbar").height();
-		console.log("visible at " + visibleHeight);
-
-		// Fix the sidebar height to the visible window
-		$("#docSidebar").css("height", visibleHeight);
-
-		// If the sidebar content is larger than visible height give it scrollability.
-		if (sidebarHeight <= visibleHeight) {
-			$("#docSidebar").css("overflow-y", "hidden");
-		} else {
-			$("#docSidebar").css("overflow-y", "scroll");
-		}
-	}
-}
-
-$(document).ready(function() {
-	$("#docSidebar").css("overflow-x", "hidden");
-	manageDocSidebar();
-	$(window).on("resize", manageDocSidebar);
-});
+// Fix anchor links in light of fixed navbar
+// http://jsfiddle.net/ianclark001/rkocah23/
 
 (function(document, history, location) {
 	var HISTORY_SUPPORT = !!(history && history.pushState);
